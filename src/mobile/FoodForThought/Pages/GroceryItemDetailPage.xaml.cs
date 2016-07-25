1dc648a823f2e832a1c6b73a1ec7c46fe3b33174
Chris Risner <chris@chrisrisner.com>
2016-07-25 15:15:41 -0700

    Showing detail page when tapping on existing items or adding a new one.  Loading fake data for now

---
+++ src/mobile/FoodForThought/Pages/GroceryItemDetailPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using FoodForThought.Models;
+using FoodForThought.ViewModels;
+using Xamarin.Forms;
+
+namespace FoodForThought.Pages
+{
+	public partial class GroceryItemDetailPage : ContentPage
+	{
+		public GroceryItemDetailPage(GroceryItem item = null)
+		{
+			InitializeComponent();
+			BindingContext = new GroceryItemDetailPageViewModel(item);
+		}
+	}
+}
+
