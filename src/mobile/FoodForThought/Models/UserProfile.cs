70aaa6dda09b299dde6d19150c9643a8b274094b
Chris Risner <chris@chrisrisner.com>
2016-07-25 13:05:22 -0700

    Added data models for User Profile and User Stats

---
+++ src/mobile/FoodForThought/Models/UserProfile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using FoodForThought.Abstractions;
+
+namespace FoodForThought.Models
+{
+	public class UserProfile : TableData
+	{
+		public string UserId { get; set; }
+		public string Password { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string Email { get; set; }
+		public string ZipCode { get; set; }
+	}
+}
+
---
+++ src/mobile/FoodForThought/Models/UserStats.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using FoodForThought.Abstractions;
+
+namespace FoodForThought.Models
+{
+	public class UserStats : TableData
+	{
+		public int ItemsDonated { get; set; }
+		public int ItemsConsumed { get; set; }
+		public int ItemsWasted { get; set; }
+		public int RecipesRecommended { get; set; }
+	}
+}
+
