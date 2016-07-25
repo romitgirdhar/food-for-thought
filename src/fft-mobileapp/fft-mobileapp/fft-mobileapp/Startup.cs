070a5247fd52bb10a98d8bf8f807da46ea5b9f0a
Romit Girdhar <rogirdh@microsoft.com>
2016-07-25 14:51:29 -0700

    Creating MobileApp Backend VS project

---
+++ src/fft-mobileapp/fft-mobileapp/fft-mobileapp/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(fft_mobileapp.Startup))]
+
+namespace fft_mobileapp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureMobileApp(app);
+        }
+    }
+}
