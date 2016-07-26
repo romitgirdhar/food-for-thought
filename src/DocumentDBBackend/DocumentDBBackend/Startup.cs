db459e83b5a9ac81f50624ce9ce60cccdfeb795d
unknown <shg@MININT-C8G1LU7.fareast.corp.microsoft.com>
2016-07-26 10:20:06 -0700

    Adding a copy of the apicontroller

---
+++ src/DocumentDBBackend/DocumentDBBackend/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(DocumentDBBackend.Startup))]
+
+namespace DocumentDBBackend
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureMobileApp(app);
+        }
+    }
+}
