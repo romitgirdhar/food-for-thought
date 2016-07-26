6325f81f33a9f0b077e96d27cc379f90d4b13a0e
Chris Risner <chris@chrisrisner.com>
2016-07-26 14:01:04 -0700

    iOS Push Notifications working

--- src/mobile/iOS/AppDelegate.cs
+++ src/mobile/iOS/AppDelegate.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-
+using Newtonsoft.Json.Linq;
 using Foundation;
 using UIKit;
+using Microsoft.WindowsAzure.MobileServices;
 
 namespace FoodForThought.iOS
 {
@@ -23,7 +24,48 @@ namespace FoodForThought.iOS
 
 			LoadApplication(new App());
 
+			// Register for push notifications.
+			var settings = UIUserNotificationSettings.GetSettingsForTypes(
+				UIUserNotificationType.Alert
+				| UIUserNotificationType.Badge
+				| UIUserNotificationType.Sound,
+				new NSSet());
+
+			UIApplication.SharedApplication.RegisterUserNotificationSettings(settings);
+			UIApplication.SharedApplication.RegisterForRemoteNotifications();
+
 			return base.FinishedLaunching(app, options);
+		}
+
+		public override void RegisteredForRemoteNotifications(UIApplication application,NSData deviceToken)
+		{
+			const string templateBodyAPNS = "{\"aps\":{\"alert\":\"$(messageParam)\"}}";
+
+			JObject templates = new JObject();
+			templates["genericMessage"] = new JObject
+				{
+				  {"body", templateBodyAPNS}
+				};
+
+			// Register for push with your mobile app
+			var push = FoodForThought.App.CloudService.GetClient().GetPush();
+			push.RegisterAsync(deviceToken, templates);
+		}
+
+		public override void DidReceiveRemoteNotification(UIApplication application,NSDictionary userInfo, Action<UIBackgroundFetchResult> completionHandler)
+		{
+			NSDictionary aps = userInfo.ObjectForKey(new NSString("aps")) as NSDictionary;
+
+			string alert = string.Empty;
+			if (aps.ContainsKey(new NSString("alert")))
+				alert = (aps[new NSString("alert")] as NSString).ToString();
+
+			//show alert
+			if (!string.IsNullOrEmpty(alert))
+			{
+				UIAlertView avAlert = new UIAlertView("Notification", alert, null, "OK", null);
+				avAlert.Show();
+			}
 		}
 	}
 }
