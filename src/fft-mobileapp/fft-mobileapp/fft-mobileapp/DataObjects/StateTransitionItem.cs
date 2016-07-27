dc54219369c68932a8b2f7586fb030e14e761bf3
Ashish Chhabria <aschhab@microsoft.com>
2016-07-27 01:35:29 +0100

    StateTransitionItem skeleton

---
+++ src/fft-mobileapp/fft-mobileapp/fft-mobileapp/DataObjects/StateTransitionItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace fft_mobileapp.DataObjects
+{
+    public class StateTransitionItem
+    {
+
+    }
+}
